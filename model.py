fa9b403c14ca48104facf302c73d42588697523e
Uvesh99 <sayaidisrar@gmail.com>
2025-06-28 22:00:39 +0530

    Removed dotenv for Streamlit Cloud

--- model.py
+++ model.py
@@ -1,11 +1,9 @@
-import os
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    api_key=os.getenv("GOOGLE_API_KEY"),
+    api_key=st.secrets["GOOGLE_API_KEY"],
     temperature=0.3
 )
+
